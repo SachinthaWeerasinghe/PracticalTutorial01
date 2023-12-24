99e718abcca540b29196188a9a7a5a532b432abe
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-24 22:42:34 +0530

    After changes in firstJavaFile.java

--- firstJavaFile.java
+++ firstJavaFile.java
@@ -1,5 +1,5 @@
 class firstJavaFile {
     public static void main(String[] args) {
-        System.out.println("This is the First Java File!"); 
+        System.out.println("This is the First Java File!, I have updated it !!!"); 
     }
 }
