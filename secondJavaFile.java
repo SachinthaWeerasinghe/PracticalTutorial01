6753dce87b6b14f182c64f32134cffd9935aa426
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-29 10:10:15 +0530

    After the update of 29 Dec

--- secondJavaFile.java
+++ secondJavaFile.java
@@ -1,5 +1,5 @@
 class secondJavaFile {
     public static void main(String[] args) {
-        System.out.println("This is the second Java File!"); 
+        System.out.println("This is the second Java File, updated 29 Dec 2023!"); 
     }
 }
