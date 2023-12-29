dda9488d4b5f8f5a46cc060cf77824282fbe0004
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-29 12:23:25 +0530

    File with two lines only

--- trial01.java
+++ trial01.java
@@ -1,5 +1,6 @@
 class trial01 {
     public static void main(String[] args) {
         System.out.println("Hi, I'm Sachintha Weerasinghe"); 
+        System.out.println("Update this before push into the GitHub")
     }
 }
