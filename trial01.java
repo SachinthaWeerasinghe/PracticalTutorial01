17fcab63ed9051c57ea61c450c9dee7bf241b363
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-29 12:25:33 +0530

    File with only single line

--- trial01.java
+++ trial01.java
@@ -1,6 +1,5 @@
 class trial01 {
     public static void main(String[] args) {
-        System.out.println("Hi, I'm Sachintha Weerasinghe"); 
-        System.out.println("Update this before push into the GitHub")
+        System.out.println("Update this before push into the GitHub");
     }
 }
