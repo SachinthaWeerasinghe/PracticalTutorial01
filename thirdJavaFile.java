1240697d3c275f04f06505a09a0034f4a9e90d2e
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-29 11:13:37 +0530

    After Editing

--- thirdJavaFile.java
+++ thirdJavaFile.java
@@ -1,5 +1,6 @@
 class thirdJavaFile {
     public static void main(String[] args) {
-        System.out.println("This is the Third Java file!"); 
+        System.out.println("This is the Third Java file!--Update the third Java File now by Sachintha Weerasinghe!!!!"); 
+	System.out.println("This is the Third Java file!--Again updated by Sachintha Weerasinghe!!!!"); 
     }
 }
